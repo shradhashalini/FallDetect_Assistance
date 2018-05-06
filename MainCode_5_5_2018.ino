6f452259329b2fd1cd954ef39fcccf8524bbcf44
Chenmengdan <34216694+chenmengdan@users.noreply.github.com>
2018-05-05 20:55:57 -0500

    Update and rename MainCode_5_5_2018 to MainCode_5_5_2018.ino

--- MainCode_5_5_2018.ino
+++ MainCode_5_5_2018.ino
@@ -2,6 +2,12 @@
 const int stepPin = 5; //CLK+
 const int dirPin = 2; //CW+ Directin of the motor
 const int enPin = 8;
+
+// rope motor
+const int stepPin2 = 6; //CLK+
+const int dirPin2 = 4; //CW+ Directin of the motor
+const int enPin2 = 3;
+
 const int ledPin = 13;
 const int interruptPin_CW = 18;
 const int interruptPin_CCW = 19;
@@ -10,7 +16,8 @@ long Y = 0;
 long Z = 0;
 bool is_reset = 0;
 int position_var;
-long target_step;
+long target_step = 0;
+long vertical_step = 0;
 
 
 //small linear actuator
@@ -24,7 +31,7 @@ long numberOfStep = 0;
 
 
 
-
+int flag = 0;
 void setup() { 
   position_var = 3;
  
@@ -33,14 +40,21 @@ void setup() {
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
   pinMode(enPin,OUTPUT);
+
+  pinMode(stepPin2,OUTPUT); 
+  pinMode(dirPin2,OUTPUT);
+  pinMode(enPin2,OUTPUT);
+
+
   pinMode(interruptPin_CW, INPUT);
   pinMode(interruptPin_CCW, INPUT);
-  digitalWrite(enPin,LOW);
+  digitalWrite(enPin,LOW);  
+  digitalWrite(enPin2,LOW);
   //attachInterrupt(digitalPinToInterrupt(interruptPin_CW), VMotor_CW, RISING);
   //attachInterrupt(digitalPinToInterrupt(interruptPin_CCW), VMotor_CCW, RISING);
 
   //small linear actuator
-   pinMode(stp, OUTPUT);
+  pinMode(stp, OUTPUT);
   pinMode(dir, OUTPUT);
   pinMode(EN, OUTPUT);
   resetEDPins(); //Set step, direction, microstep and enable pins to default states
@@ -48,46 +62,45 @@ void setup() {
 }
 void loop() {
   
-        digitalWrite(EN, LOW); //Pull enable pin low to allow motor contro
-       //StepForwardDefault();
-        resetEDPins();
-
-  if(position_var == 1) {
-    Serial.println("Position is 1");
-    target_step = 42667;
-    Serial.print("target step = ");
-    Serial.println(target_step);
-    VMotor_CW1(target_step); 
-    position_var = 0;
-
-  }
-
-    if(position_var == 2) {
-    Serial.println("Position is 2");
-    target_step = 105333;
-    Serial.print("target step = ");
-    Serial.println(target_step);
-    VMotor_CW1(target_step); 
-    position_var = 0;
-
-  }
-
+    digitalWrite(EN, LOW); //Pull enable pin low to allow motor contro
+    //StepForwardDefault();
+    
+//  if(position_var == 1) {
+//    Serial.println("Position is 1");
+//    target_step = 42667;
+//    Serial.print("target step = ");
+//    Serial.println(target_step);
+//    VMotor_CW1(target_step); 
+//    position_var = 0;
+//
+//  }
+////
+//    if(position_var == 2) {
+//    Serial.println("Position is 2");
+//    target_step = 105333;
+//    Serial.print("target step = ");
+//    Serial.println(target_step);
+//    VMotor_CW1(target_step); 
+//    position_var = 0;
+//
+//  }
+////
   if(position_var == 3) {
-    Serial.println("Position is 1");
+    Serial.println("Position is 3");
     target_step = 105333;
     Serial.print("target step = ");
     Serial.println(target_step);
-    VMotor_CW1(target_step);
     StepForwardDefault(); 
+    VMotor_CW1(target_step);
     position_var = 0;
 
   }
-  
+//  
   if(digitalRead(interruptPin_CCW) == 1 ){ // reset
     reset(); 
    
   }
-//  resetEDPins();
+  resetEDPins();
 
 }
 
@@ -116,10 +129,12 @@ void VMotor_CW1(long target_step)
     break;
    }
   }
+  vertical_motor();
 }
 
 void reset() // to go to 
 {
+  reverse_vertical_motor();
   digitalWrite(dirPin,HIGH); //MOVE TO THE RIGHT SIDE.
   // Makes 400 pulses for making two full cycle rotation
  for (X = X; X >= 0; X--) 
@@ -133,6 +148,9 @@ void reset() // to go to
     Serial.println(X);
   }
   X = 0;
+
+  ReverseStepDefault();
+  
 }
 
 //small linear actuator
@@ -147,7 +165,7 @@ void resetEDPins()
 void StepForwardDefault()
 {
   Serial.println("Moving forward at default step mode.");
-  digitalWrite(dir, HIGH); //Pull direction pin low to move "forward"
+  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
   while(numberOfStep < 66000)  //Loop the forward stepping enough times for motion to be visible
   {
     digitalWrite(stp,HIGH); //Trigger one step forward
@@ -159,7 +177,6 @@ void StepForwardDefault()
     numberOfStep++;
   }
 }
-
 //Reverse default microstep mode function
 void ReverseStepDefault()
 {
@@ -174,5 +191,48 @@ void ReverseStepDefault()
     digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
     delayMicroseconds(500);
     numberOfStep--;
+  }
+}
+
+
+void vertical_motor()
+{
+  Serial.print("Vertical step = ");
+  Serial.println(vertical_step);
+  digitalWrite(dirPin2,LOW); // MOVE TO THE LEFT SIDE.
+  // Makes 200 pulses for making one full cycle rotation
+  for (int x = 0; x <= 5000; x++) 
+  {
+    digitalWrite(stepPin2,HIGH); 
+    delayMicroseconds(1500); 
+    digitalWrite(stepPin2,LOW); 
+    delayMicroseconds(1500); 
+    Serial.print("current steps = ");
+    Serial.println(x);
+    if(digitalRead(interruptPin_CCW) == 1){
+      reset();
+      break;
+    }
+  }
+}
+void reverse_vertical_motor() 
+{
+  Serial.print("Vertical step = ");
+  Serial.println(vertical_step);
+  digitalWrite(dirPin2,HIGH); // MOVE TO THE LEFT SIDE.
+  // Makes 200 pulses for making one full cycle rotation
+  for (int x = 5000; x >= 0; x--) 
+  {
+    digitalWrite(stepPin2,HIGH); 
+    delayMicroseconds(1500); 
+    digitalWrite(stepPin2,LOW); 
+    delayMicroseconds(1500); 
+    Serial.print("current steps = ");
+    Serial.println(x);
+    
+    if(digitalRead(interruptPin_CCW) == 1){
+    reset();
+    break;
+   }
   }
 }
